383ba675a51c3a52f771f8a8fa898420243b33d8
piyushhawq <56574565+piyushhawq@users.noreply.github.com>
2022-07-24 20:29:36 +0530

    Updates

---
+++ precache-manifest.b107c6892bb390ed10656d803960992d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4056b358c498d3b452bbc2a10503e992",
+    "url": "/Tooltip_pc/index.html"
+  },
+  {
+    "revision": "be5fa85b057020b6cdf0",
+    "url": "/Tooltip_pc/static/css/main.5d4eb3ba.chunk.css"
+  },
+  {
+    "revision": "39f2c1537d206e9168ea",
+    "url": "/Tooltip_pc/static/js/2.0e2e7a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tooltip_pc/static/js/2.0e2e7a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5fa85b057020b6cdf0",
+    "url": "/Tooltip_pc/static/js/main.8fb3ec7e.chunk.js"
+  },
+  {
+    "revision": "8bc6b57c90d1147813a3",
+    "url": "/Tooltip_pc/static/js/runtime-main.4b72f6be.js"
+  }
+]);
